affdf443b5bc35b081fcc574ff9a93a4ce3e7241
Kaiet <kjimeneald@educacion.navarra.es>
2023-11-09 18:44:22 +0100

    seguido

--- Orla de Kaiet/obj/Debug/Orla de Kaiet_Content.g.i.cs
+++ Orla de Kaiet/obj/Debug/Orla de Kaiet_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenes/ordenador.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenes/randy.jpg")]
 
 
