8125fef34cac21aa9d25e4c3b41e2304a55f6336
Kaiet <kjimeneald@educacion.navarra.es>
2023-11-28 20:33:33 +0100

    seguir

--- GestionEmpleados2023/AgregarEmpleado.xaml.cs
+++ GestionEmpleados2023/AgregarEmpleado.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace GestionEmpleados2023
+namespace cc
 {
     /// <summary>
     /// Lógica de interacción para AgregarEmpleado.xaml
@@ -42,9 +44,20 @@ namespace GestionEmpleados2023
         }
 
         private void AgregarEmpleadoString(string nombe, string apellidos, bool esUsuario, int edad) {
-            using (SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings[]))
+            using (SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["AgregarEmpleadoString.Properties.Settings.GestorEmpleadosEjerConnectionString"].ConnectionString)) {
+
+                string consulta = "INSERT INTO EMPLEADOS (Nombre, Apellidos, EsUsuario, Edad) VALUES (@nombre, @apellidos, @Apellidos, @EsUsuario, @Edad)";
+
+                using (SqlCommand cnd = new SqlCommand(consulta,conexion) ) {
+                    cnd.Parameters.AddWithValue("@Nombre", nombe);
+                
+                
+                }
+            
+            
+            }
         
-        
+            
         }
     }
     
