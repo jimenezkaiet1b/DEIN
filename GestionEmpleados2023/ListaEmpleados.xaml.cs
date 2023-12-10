9fb351a3b192b5228499216724d71776a480b81b
kaiet <kjimeneald@educacion.navarra.es>
2023-12-10 17:15:49 +0100

    acabao

--- GestionEmpleados2023/ListaEmpleados.xaml.cs
+++ GestionEmpleados2023/ListaEmpleados.xaml.cs
@@ -37,10 +37,50 @@ namespace GestionEmpleados2023
         }
 
 
+        private void borrarEmpleado(string nombre)
+        {
+            using (SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["GestionEmpleados2023.Properties.Settings.GestionEmpleadosConnectionString"].ConnectionString))
+            {
+                string consulta = "DELETE FROM EMPLEADOS WHERE nombre = @Nombre";
 
+                using (SqlCommand cmd = new SqlCommand(consulta, conexion))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", nombre);
 
+                    try
+                    {
+                        conexion.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al eliminar empleado: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+
+        private void eliminar_click(object sender, RoutedEventArgs e)
+        {
+            int selectedIndex = dataGrid.SelectedIndex;
+
+            if (selectedIndex >= 0)
+            {
+                Empleado empleadoSeleccionado = dataGrid.SelectedItem as Empleado;
+                borrarEmpleado(empleadoSeleccionado.Nombre);
+
+                cargarEmpleadosEnDataGrid();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un registro para eliminar.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
     }
 
 
 
 }
+
+
+
